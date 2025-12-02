079a9b1c6df1a829c5b55d7e2643a18a17526cff
git-user-ui <pratikpp2302@gmail.com>
2025-12-02 15:50:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+	const createlemntP = document.createElement("p");
+	createlemntP.textContent = "DOM load success";
+
+	document.body.appendChild(createlemntP)
+})
